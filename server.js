96287cd859aecf0fec6369b1b8996e9037ba5508
Luis Davi <luisdavi1063@gmail.com>
2020-12-05 22:29:33 -0300

    Painel e sistema quase pronto de login

--- server.js
+++ server.js
@@ -7,35 +7,53 @@ const Cryptr = new crypt('minhasenha')
 const MongoClient = require('mongodb').MongoClient;
 const uri = 'mongodb+srv://LuisDavi:jaclecle12@cluster0.sn7jc.mongodb.net/Crud-Api?retryWrites=true&w=majority';
 
-app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 const client = new MongoClient (uri, {useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect (err => {
     if (err) return console.log(err);
-
     db = client.db('Crud-Api');
-    app.listen(9032, () => console.log("Server running on port 9032"));
+    app.listen(9032, () => console.log("Server running on port 9032"))
 });
-
 app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.post('/show', (req, res) => {
-    //criptografa o req.body
-    const dateCrypt = Cryptr.encrypt(req.body.name);
-    //req.body.name = ao .name criptografado
-    req.body.name = dateCrypt;
 
+app.post('/auth', (req, res)=>{
+    db.collection('data').find().toArray((err, result)=>{
+        for(var user of result){
+            if(req.body.emailLogin == user.emailData){
+                if(req.body.passwordLogin == user.passwordData){
+                    console.log("senha correta")
+                }
+            }
+        }
+    })
+    //res.redirect('/')
+})
+
+app.get('/login', (req, res)=>{
+    res.render("show.ejs")
+})
+app.post('/show', (req, res) => {
+    //criptografa o req.body 
+    //const dateCrypt = Cryptr.encrypt(req.body.name);
+    //req.body.name = name criptografado
+    //req.body.name = dateCrypt;
     //Salva no banco de dados
     db.collection('data').save(req.body, (err, result)=>{
        if(err) return console.log(err);
         //avisa que salvou no banco de dados
-        console.log('Saved in data base');
-        res.redirect('/');
+        console.log('Saved in data base');""
+        console.log()
+        res.redirect('/login');
    });
 });
+
+
